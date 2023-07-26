bd98cd4d13bc19c47e9fefb4f5aa75bc578e9048
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-07-26 18:11:58 +0100

    push and pall

---
+++ push.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+/**
+ * push - add node at the beg of stack
+ * @stack: head of the stack
+ * @line_number: void
+ * Return: nothing
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+	(void)line_number;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = PUSH_VALUE;
+	new->prev = NULL;
+	new->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new;
+
+	*stack = new;
+}
