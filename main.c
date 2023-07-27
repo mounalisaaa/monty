6d3b7a7a885050961348e6ced5dec797a35af4bd
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-07-27 23:54:26 +0100

    if !file

--- main.c
+++ main.c
@@ -19,6 +19,11 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 	while (fgets(buffer, 1024, file))
 	{
 		line_number++;
