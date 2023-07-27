c4ff60efb60628a9182f26c17c3c4567e967eb94
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-07-27 23:05:27 +0100

    .

--- main_helper.c
+++ main_helper.c
@@ -1,28 +1,15 @@
 #include "monty.h"
-
 int PUSH_VALUE;
-
+/**
+ * handle_opcode - to execute
+ * @opcode: the opcode
+ * @stack: the head
+ * @line_number: number of lines
+ */
 void handle_opcode(char *opcode, stack_t **stack, int line_number)
 {
-	int n;
-
 	if (strcmp(opcode, "push") == 0)
-	{
-		opcode = strtok(NULL, " \t\n");
-		if (!opcode)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-		if (check_int(opcode) == 1)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-		n = atoi(opcode);
-		PUSH_VALUE = n;
-		push(stack, line_number);
-	}
+		pushing(opcode, stack, line_number);
 	else if (strcmp(opcode, "pall") == 0)
 		pall(stack, line_number);
 	else
@@ -30,6 +17,31 @@ void handle_opcode(char *opcode, stack_t **stack, int line_number)
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 		exit(EXIT_FAILURE);
 	}
+}
+/**
+ * pushing - push helper
+ * @opcode: the opcode
+ * @stack: the head
+ * @line_number: number of lines
+ */
+void pushing(char *opcode, stack_t **stack, int line_number)
+{
+	int n;
+
+	opcode = strtok(NULL, " \t\n");
+	if (!opcode)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (check_int(opcode) == 1)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(opcode);
+	PUSH_VALUE = n;
+	push(stack, line_number);
 }
 /**
  * check_int - check if a string is consist of integers only
--- monty.h
+++ monty.h
@@ -7,7 +7,6 @@
 
 extern int PUSH_VALUE;
 
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -38,13 +37,12 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
-
 
 /**main**/
 void handle_opcode(char *opcode, stack_t **stack, int line_number);
 int check_int(char *str);
+void pushing(char *opcode, stack_t **stack, int line_number);
 
 #endif
